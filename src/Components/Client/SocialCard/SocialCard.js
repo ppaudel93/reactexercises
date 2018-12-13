5ffbe9c4240b640b48efc536d6575a887da9399c
Prayog Paudel <ppaudel93@gmail.com>
2018-12-13 20:27:32 +0545

    Cleaned up boilerplate

---
+++ src/Components/Client/SocialCard/SocialCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class SocialCard extends Component {
+  state = {};
+  render() {
+    return <div className="card" />;
+  }
+}
+
+export default SocialCard;
